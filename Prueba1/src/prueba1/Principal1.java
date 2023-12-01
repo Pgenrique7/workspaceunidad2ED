80d32f8f4d0390caba04a514b13ea682d5a6d70b
2DAW <2DAW@2DAM-19>
2023-12-01 12:45:59 +0100

    Version 1.0.1 01/12/2023

---
+++ Prueba1/src/prueba1/Principal1.java
@@ -0,0 +1,17 @@
+package prueba1;
+
+public class Principal1 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hola mundo java-git");
+		System.out.println("Hola mundo java-git");
+		System.out.println("Hola mundo java-git");
+		System.out.println("Hola mundo java-git");
+		System.out.println("Pruebas git. Se añade cambio1");
+		
+
+	}
+
+}
