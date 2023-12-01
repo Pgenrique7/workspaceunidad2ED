d8100ba4dce5a86f6868116bdb1fc30a4973f314
2DAW <2DAW@2DAM-19>
2023-12-01 12:47:39 +0100

    Version 1.0.2 01/12/2023

--- Prueba1/src/prueba1/cambio1.java
+++ Prueba1/src/prueba1/cambio1.java
@@ -5,6 +5,7 @@ public class cambio1 {
 	public static void main(String[] args) {
 			
 		System.out.println("Pruebas Git . Clase cambio1");
+		System.out.println("Pruebas Git . Clase cambio1. Añadimos cambio para comparar");
 	}
 
 }
